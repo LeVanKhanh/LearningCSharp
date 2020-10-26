bba343f5d7391e3567d831f279d7bae7dceb7156
Van Khanh LE <vle2@amaris.com>
2020-10-26 08:57:49 +0700

    add file

---
+++ Principes/Principes/Open_Closed/Correct/Ocp_Correct_Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Principes.Open_Closed.Correct
+{
+    public class Ocp_Correct_Test : ITest
+    {
+        public void Test()
+        {
+            
+        }
+    }
+}
